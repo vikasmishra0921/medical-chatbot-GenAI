ec57902aeb362b710190a938ddd00c324b8e6eca
Vikasmishra0921 <mishravikas7219@gmail.com>
2026-04-21 20:43:54 +0530

    requirements added setup.py updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    namme='medical-chatbot-GenAI',
+    version='0.1.0',
+    author="Vikas Mishra",
+    author_email="Mishravikas8087@gmail.com",
+    description="A medical chatbot using GenAI",
+    packages=find_packages(),
+    install_requires=[]
+)
